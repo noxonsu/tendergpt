601f101ef4432514c09461b55ee522f784bffc47
Noxon <2914674+noxonsu@users.noreply.github.com>
2023-10-18 14:49:29 +0000

    to action

--- 6mysqldon.py
+++ 6mysqldon.py
@@ -3,10 +3,21 @@ import requests
 import json
 import time
 from datetime import datetime, timedelta
+from langchain.chat_models import ChatOpenAI
+from langchain.prompts.chat import (
+    ChatPromptTemplate,
+    SystemMessagePromptTemplate,
+    AIMessagePromptTemplate,
+    HumanMessagePromptTemplate,
+)
+from langchain.schema import AIMessage, HumanMessage, SystemMessage
+import requests
+import json
+chat = ChatOpenAI(temperature=0,model_name="gpt-4")
 
 BASE_URL = "https://apitorgi.myseldon.com"
 LOGIN_ENDPOINT = "/User/Login"
-CREATE_PURCHASE_REQUEST_ENDPOINT = "/Purchases/New"  
+CREATE_PURCHASE_REQUEST_ENDPOINT = "/Purchases/Update"  
 FETCH_PURCHASES_RESULTS_ENDPOINT = "/Purchases/Result"
 
 def login_to_service():
@@ -31,7 +42,7 @@ def login_to_service():
 def request_purchase_data(token):
     purchase_request_url = f"{BASE_URL}{CREATE_PURCHASE_REQUEST_ENDPOINT}?token={token}"
     
-    date_ago = (datetime.now() - timedelta(days=20)).strftime('%Y-%m-%dT%H:%M:%S.%f')[:-3] + "Z"
+    date_ago = (datetime.now() - timedelta(days=30)).strftime('%Y-%m-%dT%H:%M:%S.%f')[:-3] + "Z"
     date_to = datetime.now().strftime('%Y-%m-%dT%H:%M:%S.%f')[:-3] + "Z"
 
     payload = {
@@ -94,14 +105,17 @@ def fetch_purchases_results(token, task_id):
         'Content-Type': 'application/json',
         'Accept': 'application/json'
     }
-    
+    #load prompt.txt
+    with open("prompt.txt", "r") as f:
+        prompt = f.read()
+        f.close()
     try:
         response = requests.post(result_url, json=payload, headers=headers)
         if response.status_code != 200:
             raise ValueError("Failed to fetch purchases results!")
 
         # Debug print
-        print(f"Response JSON: {response.json()}")
+        #print(f"Response JSON: {response.json()}")
 
         purchase_data = response.json()
         if 'result' not in purchase_data or 'purchases' not in purchase_data['result']:
@@ -114,8 +128,45 @@ def fetch_purchases_results(token, task_id):
 
         for purchase in purchases:
             seldon_id = purchase['SeldonId']
+            purchasetext = json.dumps(purchase, ensure_ascii=False)
+            
+            print(seldon_id)
+
             with open(f'purchases/{seldon_id}.json', 'w') as outfile:
                 json.dump(purchase, outfile)
+            
+            print(len(purchasetext))
+            if (len(purchasetext) > 30000): 
+                print(" Too long ")
+                #remove lotsList field from purchase
+                purchase.pop('lotsList', None)
+                purchasetext = json.dumps(purchase, ensure_ascii=False)
+                print(len(purchasetext))
+
+            messages = [
+                SystemMessage(
+                    content=prompt
+                ),
+                HumanMessage(
+                    content=purchasetext
+                )
+            ]
+
+            gpttitle = chat(messages)
+            #wait 60 sec
+            time.sleep(20)
+
+            print(gpttitle.content)
+            
+
+            if gpttitle.content is not None and len(gpttitle.content) > 20: # and "Не подходит" not in gpttitle.content
+                
+                gpttitle.content = gpttitle.content.replace("&", "%26")
+                webhook_url = f"https://noxon.wpmix.net/counter.php?totenders=1&msg={gpttitle.content}"              
+                response = requests.post(webhook_url)
+                if response.status_code != 200:
+                    print(f"Failed to send webhook for tender at index {idx}. Status code: {response.status_code}")
+
 
     except Exception as e:
         print(f"An error occurred: {e}")
